4ebfac2f88710fdfeffeb00be5cd36e9204d01fd
Nevil George <nevilgeorge2016@u.northwestern.edu>
2014-09-28 21:55:51 -0500

    changes to notes

---
+++ app/scripts/controller.js
@@ -0,0 +1,25 @@
+'use strict';
+var taskstarterControllers = angular.module('taskstarterControllers', []);
+/**
+ * @ngdoc function
+ * @name taskstarterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the taskstarterApp
+ */
+taskstarterControllers.controller('MainCtrl', function ($scope) {
+    $scope.todos = [
+    	{
+    		'text': 'Tell Christina to shut up.',
+    		'priority': 2
+    	},
+    	{
+    		'text': 'Do laundry',
+    		'priority': 5
+    	},
+    	{
+    		'text': 'Buy groceries.',
+    		'priority': 3
+    	}
+    ];
+  });
