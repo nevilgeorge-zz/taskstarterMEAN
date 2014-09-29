5430135be4e1c83194dfc32b28637a0f8efb12fb
Nevil George <nevilgeorge2016@u.northwestern.edu>
2014-09-29 00:13:39 -0500

    todo list updating working without http requests

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,19 +1,23 @@
 'use strict';
 var taskstarterControllers = angular.module('taskstarterControllers', []);
 var todoList = [
-        {
-            'text': 'Tell Christina to shut up.',
-            'priority': 2
-        },
-        {
-            'text': 'Do laundry',
-            'priority': 5
-        },
-        {
-            'text': 'Buy groceries.',
-            'priority': 3
-        }
-    ];
+    {
+        "text": "Tell Christina to shut up.",
+        "priority": 2
+    },
+    {
+        "text": "Do laundry",
+        "priority": 5
+    },
+    {
+        "text": "Buy groceries.",
+        "priority": 3
+    },
+    {
+        "text": "Eat lunch",
+        "priority": 8
+    }
+];
 /**
  * @ngdoc function
  * @name taskstarterApp.controller:MainCtrl
@@ -21,22 +25,25 @@ var todoList = [
  * # MainCtrl
  * Controller of the taskstarterApp
  */
-taskstarterControllers.controller('MainCtrl', ['$scope', function($scope) {
+taskstarterControllers.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
+    // $http.get('todos.json').success(function(data) {
+    //     $scope.todos = data;
+    // });
     $scope.todos = todoList;
-  }]);
+}]);
 
-taskstarterControllers.controller('FormCtrl', ['$scope', function($scope) {
-    $scope.print = function() {
-        var temp = {
-            'text': $scope.todo,
-            'priority': 0
+taskstarterControllers.controller('FormCtrl', ['$scope', '$http', function($scope, $http) {
+    var temp, reset;
+    $scope.save = function() {
+        temp = {
+            "text": $scope.todoText,
+            "priority": 0
         };
         todoList.push(temp);
-        console.log(todoList);
         reset();
     }
 
-    var reset = function() {
+    reset = function() {
         $scope.todo = "";
     }
 }]);
