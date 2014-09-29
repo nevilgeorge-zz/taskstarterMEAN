e0e3b3de4faa2be8f04cddc64c617ff08feb65a3
Nevil George <nevilgeorge2016@u.northwestern.edu>
2014-09-28 23:27:56 -0500

    added controller logic to addform.html

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,5 +1,19 @@
 'use strict';
 var taskstarterControllers = angular.module('taskstarterControllers', []);
+var todoList = [
+        {
+            'text': 'Tell Christina to shut up.',
+            'priority': 2
+        },
+        {
+            'text': 'Do laundry',
+            'priority': 5
+        },
+        {
+            'text': 'Buy groceries.',
+            'priority': 3
+        }
+    ];
 /**
  * @ngdoc function
  * @name taskstarterApp.controller:MainCtrl
@@ -8,20 +22,22 @@ var taskstarterControllers = angular.module('taskstarterControllers', []);
  * Controller of the taskstarterApp
  */
 taskstarterControllers.controller('MainCtrl', ['$scope', function($scope) {
-    $scope.todos = [
-    	{
-    		'text': 'Tell Christina to shut up.',
-    		'priority': 2
-    	},
-    	{
-    		'text': 'Do laundry',
-    		'priority': 5
-    	},
-    	{
-    		'text': 'Buy groceries.',
-    		'priority': 3
-    	}
-    ];
+    $scope.todos = todoList;
   }]);
 
+taskstarterControllers.controller('FormCtrl', ['$scope', function($scope) {
+    $scope.print = function() {
+        var temp = {
+            'text': $scope.todo,
+            'priority': 0
+        };
+        todoList.push(temp);
+        console.log(todoList);
+        reset();
+    }
+
+    var reset = function() {
+        $scope.todo = "";
+    }
+}]);
 
